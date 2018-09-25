07b7283f5b2e9081b2ed0eaa555b8ed4824d3535
João Lucas <joao.frhb@gmail.com>
2018-09-24 21:29:57 -0300

    versao 1 finalizada

---
+++ js/codigo.js
@@ -0,0 +1,8 @@
+function calcularSoma(){
+	let n1 = document.getElementById("n1").value;
+	let n2 = document.getElementById("n2").value;
+
+	let resultado = document.getElementById("resultado")
+
+	resultado.innerHTML = parseFloat(n1)+parseFloat(n2);
+}
